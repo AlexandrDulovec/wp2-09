df93457c1ecafec3c95c7423978b9d84371783dc
AlexandrDulovec <79139997+AlexandrDulovec@users.noreply.github.com>
2022-01-06 11:02:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Kostky</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>getPES</title>
 </head>
 <body>
-<h1>Kostky</h1>
-<?php 
-$diceCount = 8;
-function sumDice(int $diceCount) {
-$summary = 0;
-for ($i=0; $i < $diceCount ; $i++) { 
-$summary += rand(1,6);
-}
-return $summary;
-}
-echo sumDice(2);
- ?>
+    <?php
+    $pocetnakazenych = rand(1, 250000);
+    function getPES(int $pocetnakazenych){
+        if( $pocetnakazenych <= 10000 ) {$pes = 1;}
+        elseif(( $pocetnakazenych > 10000 ) && ( $pocetnakazenych <= 50000)) {$pes = 2;}
+        elseif(( $pocetnakazenych > 50000 ) && ( $pocetnakazenych <= 100000)) {$pes = 3;}
+        elseif(($pocetnakazenych > 100000 ) && ( $pocetnakazenych <= 200000)) {$pes = 4;}
+        elseif($pocetnakazenych > 200000) {$pes = 5;}
+        return $pes;
+    }
+    echo getPES($nakazeni)
+    ?>
+    <?php echo getPES(10000); // vypíše 1 ?><br>
+    <?php echo getPES(50000); // vypíše 2 ?><br>
+    <?php echo getPES(100001); // vypíše 4 ?><br>
 </body>
 </html> 
